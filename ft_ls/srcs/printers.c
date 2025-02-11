4f62ed55cfc37c6db9320d2ecdb54cf90cf84051
Marco177171 <msebastiani93@gmail.com>
2025-02-11 21:33:26 +0100

    update

--- ft_ls/srcs/printers.c
+++ ft_ls/srcs/printers.c
@@ -24,14 +24,13 @@ void print_file_creation_time(t_entry entry) {
 void print_this_entry(t_entry *entry, t_command *command_structure, int index) {
 	if (find_in_string(command_structure->flags, 'l') == 1) {
 		struct passwd *pwd = getpwuid(entry[index].fileStat->st_uid);
-		struct group *grp = getgrgid(entry[index].fileStat->st_uid); // might present issues with clulsters machines
+		// struct group *grp = getgrgid(entry[index].fileStat->st_uid); // might present issues with clulsters machines
 
 		print_file_data(entry[index]);
 
 		if (find_in_string(command_structure->flags, 'g') != 1) // might present issues with clulsters machines
 			write(1, strcat(pwd->pw_name, " "), ft_strlen(pwd->pw_name) + 1);
-		
-		write(1, strcat(grp->gr_name, " "), ft_strlen(grp->gr_name) + 1);
+		// write(1, strcat(grp->gr_name, " "), ft_strlen(grp->gr_name) + 1);
 
 		char *subfolders_amount = ft_itoa((int)entry[index].fileStat->st_nlink);
 		int subfolders_len = ft_strlen(subfolders_amount);
