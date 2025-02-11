e2d1ac23572c62101cb56df2400a3417240dd2eb
Marco177171 <msebastiani93@gmail.com>
2025-02-11 18:50:00 +0100

    subfolders amount gets printed with -l flag

--- ft_ls/srcs/printers.c
+++ ft_ls/srcs/printers.c
@@ -25,10 +25,19 @@ void print_this_entry(t_entry *entry, t_command *command_structure, int index) {
 	if (find_in_string(command_structure->flags, 'l') == 1) {
 		struct passwd *pwd = getpwuid(entry[index].fileStat->st_uid);
 		struct group *grp = getgrgid(entry[index].fileStat->st_uid);
+
 		print_file_data(entry[index]);
+
 		if (find_in_string(command_structure->flags, 'g') != 1)
 			write(1, strcat(pwd->pw_name, " "), ft_strlen(pwd->pw_name) + 1);
+		
 		write(1, strcat(grp->gr_name, " "), ft_strlen(grp->gr_name) + 1);
+
+		char *subfolders_amount = ft_itoa((int)entry[index].fileStat->st_nlink);
+		int subfolders_len = ft_strlen(subfolders_amount);
+		write(1, strcat(subfolders_amount, " "), subfolders_len + 1);
+		free(subfolders_amount);
+		
 		print_file_creation_time(entry[index]);
 		
 		char *file_size = ft_itoa((int)(entry[index].fileStat->st_size));
